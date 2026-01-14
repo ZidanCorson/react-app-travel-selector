46d3449d9244d4d6db8efcb0f94c26a2fff1bcbb
ZidanCorson <zwang@scottlogic.com>
2026-01-14 09:24:53 +0000

    adds data specifincs of emergency data

---
+++ src/data/cities_emergency.ts
@@ -0,0 +1,66 @@
+
+export const cityEmergencyData: { [key: string]: { police: string; ambulance: string; fire: string; general: string; hospital: string } } = {
+  "New York": {
+    police: "911",
+    ambulance: "911",
+    fire: "911",
+    general: "911",
+    hospital: "New York Presbyterian Hospital: (212) 746-5454"
+  },
+  "San Francisco": {
+    police: "911",
+    ambulance: "911",
+    fire: "911",
+    general: "911",
+    hospital: "Zuckerberg San Francisco General: (628) 206-8000"
+  },
+  "Tokyo": {
+    police: "110",
+    ambulance: "119",
+    fire: "119",
+    general: "110 / 119",
+    hospital: "St. Luke's International Hospital: 03-3541-5151"
+  },
+  "Paris": {
+    police: "17",
+    ambulance: "15",
+    fire: "18",
+    general: "112 (EU wide)",
+    hospital: "American Hospital of Paris: +33 1 46 41 25 25"
+  },
+  "London": {
+    police: "999",
+    ambulance: "999",
+    fire: "999",
+    general: "999 / 112",
+    hospital: "St Thomas' Hospital: 020 7188 7188"
+  },
+  "Marrakech": {
+    police: "19",
+    ambulance: "15",
+    fire: "15",
+    general: "19 / 15",
+    hospital: "Clinique Internationale Marrakech: +212 5243-69595"
+  },
+  "Kyoto": {
+    police: "110",
+    ambulance: "119",
+    fire: "119",
+    general: "110 / 119",
+    hospital: "Kyoto University Hospital: 075-751-3111"
+  },
+  "Casablanca": {
+    police: "19",
+    ambulance: "15",
+    fire: "15",
+    general: "19 / 15",
+    hospital: "Cheikh Khalifa International University Hospital: +212 5290-04488"
+  },
+  "Beijing": {
+    police: "110",
+    ambulance: "120",
+    fire: "119",
+    general: "110",
+    hospital: "Beijing United Family Hospital: +86 10 5927 7000"
+  }
+};
