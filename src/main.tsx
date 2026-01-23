027ff0c7fb07053a5795caf471f23eaffdaca4a2
Zidan Wang <zidan.wang@bbc.co.uk>
2026-01-23 09:03:46 +0000

    fixes error

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
-import ErrorBoundary from './components/ErrorBoundary.tsx'
+import App from './App'
+import ErrorBoundary from './components/ErrorBoundary'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
