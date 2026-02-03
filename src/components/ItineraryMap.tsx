13b5b4746ebc649a64001d321f7d1dc72344d289
Zidan Wang <zidan.wang@bbc.co.uk>
2026-02-03 09:32:29 +0000

    adds a simple map component

---
+++ src/components/ItineraryMap.tsx
@@ -0,0 +1,5 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+// This file exports a simple map component for reuse
+export { MapContainer, TileLayer, Marker, Popup };
